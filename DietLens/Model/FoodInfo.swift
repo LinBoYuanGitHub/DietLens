cf716f65342cc41228c5e9483453cbd2f983af1c
LinBoYuan <boyuan_lin@yahoo.com>
2017-11-13 16:23:17 +0800

    commit realm framework

---
+++ DietLens/Model/FoodInfo.swift
@@ -0,0 +1,24 @@
+//
+//  FoodInfo.swift
+//  DietLens
+//
+//  Created by linby on 08/11/2017.
+//  Copyright © 2017 NExT++. All rights reserved.
+//
+
+import Foundation
+
+struct FoodInfo{
+    public var food_id:String = ""
+    public var rank:Int = 0
+    public var food_name:String = ""
+    public var carbohydrate:String = ""
+    public var protein:String = ""
+    public var fat:String = ""
+    public var calorie:Float = 0.0
+    
+    init(){
+        
+    }
+    
+}
