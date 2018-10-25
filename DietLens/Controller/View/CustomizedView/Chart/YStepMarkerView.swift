7b3cee2330014c571877061457d4602c2a073a44
LinBoYuan <boyuan_lin@yahoo.com>
2018-10-25 10:45:06 +0800

    add YStepMarkerView for chartView.

--- DietLens/Controller/View/CustomizedView/Chart/YStepMarkerView.swift
+++ DietLens/Controller/View/CustomizedView/Chart/YStepMarkerView.swift
@@ -9,18 +9,168 @@
 import Foundation
 import Charts
 
-public class YStepMarkerView: BalloonMarker {
-    fileprivate var yFormatter = NumberFormatter()
+open class YStepMarkerView: MarkerImage {
+    open var color: UIColor
+    open var arrowSize = CGSize(width: 15, height: 11)
 
-    public override init(color: UIColor, font: UIFont, textColor: UIColor, insets: UIEdgeInsets) {
-        yFormatter.minimumFractionDigits = 1
-        yFormatter.maximumFractionDigits = 1
-        super.init(color: color, font: font, textColor: textColor, insets: insets)
+    open var font: UIFont
+    open var textColor: UIColor
+    open var insets: UIEdgeInsets
+    open var minimumSize = CGSize()
+
+    fileprivate var label: String?
+    fileprivate var _labelSize: CGSize = CGSize()
+    fileprivate var _paragraphStyle: NSMutableParagraphStyle?
+    fileprivate var _drawAttributes = [NSAttributedString.Key: AnyObject]()
+
+    let labelHeight = 70 //at the top of the chartView
+    let lineHeight = 460
+
+    public init(color: UIColor, font: UIFont, textColor: UIColor, insets: UIEdgeInsets) {
+        self.color = color
+        self.font = font
+        self.textColor = textColor
+        self.insets = insets
+
+        _paragraphStyle = NSParagraphStyle.default.mutableCopy() as? NSMutableParagraphStyle
+        _paragraphStyle?.alignment = .center
+        super.init()
     }
 
-    public override func refreshContent(entry: ChartDataEntry, highlight: Highlight) {
-        let string = String(Int(floor(entry.y)))
-        setLabel(string)
+    open override func offsetForDrawing(atPoint point: CGPoint) -> CGPoint {
+        var offset = self.offset
+        var size = self.size
+
+        if size.width == 0.0 && image != nil {
+            size.width = image!.size.width
+        }
+        if size.height == 0.0 && image != nil {
+            size.height = image!.size.height
+        }
+
+        let width = size.width
+        let height = size.height
+        let padding: CGFloat = 8.0
+
+        var origin = point
+        origin.x -= width / 2
+        origin.y -= height
+
+        if origin.x + offset.x < 0.0 {
+            offset.x = -origin.x + padding
+        } else if let chart = chartView,
+            origin.x + width + offset.x > chart.bounds.size.width {
+            offset.x = chart.bounds.size.width - origin.x - width - padding
+        }
+
+        if origin.y + offset.y < 0 {
+            offset.y = height + padding
+        } else if let chart = chartView,
+            origin.y + height + offset.y > chart.bounds.size.height {
+            offset.y = chart.bounds.size.height - origin.y - height - padding
+        }
+        return offset
+    }
+
+    open override func draw(context: CGContext, point: CGPoint) {
+        guard let label = label else { return }
+
+        let offset = self.offsetForDrawing(atPoint: point)
+        let size = self.size
+
+        var rect = CGRect(
+            origin: CGPoint(
+                x: point.x + offset.x,
+                y: CGFloat(labelHeight)),
+            size: size)
+        rect.origin.x -= size.width / 2.0
+        rect.origin.y -= size.height
+
+        context.saveGState()
+
+        context.setFillColor(color.cgColor)
+
+        if offset.y > 0 {
+            context.beginPath()
+            //left bottom rect point
+            context.move(to: CGPoint(
+                x: rect.origin.x,
+                y: rect.origin.y + CGFloat(lineHeight)))
+            //darw rect
+            context.addLine(to: CGPoint(
+                x: rect.origin.x + rect.size.width/2,
+                y: rect.origin.y + CGFloat(lineHeight)))
+            context.addLine(to: CGPoint(
+                x: rect.origin.x + rect.size.width/2,
+                y: 500))
+            context.addLine(to: CGPoint(
+                x: rect.origin.x + rect.size.width/2,
+                y: rect.origin.y + CGFloat(lineHeight)))
+            context.addLine(to: CGPoint(
+                x: rect.origin.x + rect.size.width/2,
+                y: rect.origin.y + CGFloat(lineHeight)))
+            //the other 3 lines
+            context.addLine(to: CGPoint(
+                x: rect.origin.x + rect.size.width,
+                y: rect.origin.y))
+            context.addLine(to: CGPoint(
+                x: rect.origin.x,
+                y: rect.origin.y))
+            context.addLine(to: CGPoint(
+                x: rect.origin.x,
+                y: rect.origin.y +  CGFloat(lineHeight)))
+            context.fillPath()
+            //draw line
+            context.beginPath()
+            context.move(to: CGPoint(
+                x: rect.origin.x/2,
+                y: rect.origin.y))
+            context.addLine(to: CGPoint(
+                x: rect.origin.x/2,
+                y: 0))
+            context.fillPath()
+        } else {
+            context.beginPath()
+            //darw rect
+            context.addRect(CGRect(x: rect.origin.x, y: rect.origin.y, width: rect.size.width, height: rect.size.height/2))
+            //draw line
+            context.addRect(CGRect(x: rect.origin.x + rect.size.width/2, y: rect.origin.y + rect.size.height/2, width: 1, height: 460))
+            context.fillPath()
+        }
+
+//        if offset.y > 0 {
+//            rect.origin.y += self.insets.top + arrowSize.height
+//        } else {
+//            rect.origin.y += self.insets.top
+//        }
+//        rect.size.height -= self.insets.top + self.insets.bottom
+
+        UIGraphicsPushContext(context)
+        label.draw(in: rect, withAttributes: _drawAttributes)
+        UIGraphicsPopContext()
+        context.restoreGState()
+    }
+
+    open override func refreshContent(entry: ChartDataEntry, highlight: Highlight) {
+        setLabel(String(Int(entry.y)))
+    }
+
+    open func setLabel(_ newLabel: String) {
+        label = newLabel
+
+        _drawAttributes.removeAll()
+        _drawAttributes[.font] = self.font
+        _drawAttributes[.paragraphStyle] = _paragraphStyle
+        _drawAttributes[.foregroundColor] = self.textColor
+
+        _labelSize = label?.size(withAttributes: _drawAttributes) ?? CGSize.zero
+
+        var size = CGSize()
+        size.width = _labelSize.width + self.insets.left + self.insets.right
+        size.height = _labelSize.height + self.insets.top + self.insets.bottom
+        size.width = max(minimumSize.width, size.width)
+        size.height = max(minimumSize.height, size.height)
+        self.size = size
     }
 
 }
