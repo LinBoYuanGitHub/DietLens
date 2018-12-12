879d1421a095a34ae5af7d40e6ce3b695e87b756
MaC18 <MaCong19940421@gmail.com>
2018-12-12 14:44:05 +0800

    the page of eConsent is done by new design

--- DietLens/Controller/ClinicalStudies/EConsentViewController.swift
+++ DietLens/Controller/ClinicalStudies/EConsentViewController.swift
@@ -11,12 +11,10 @@ import UIKit
 class EConsentViewController: UIViewController {
 
     @IBOutlet weak var nextbutton: UIButton!
-    let code = VerificationCodeView(frame: CGRect(x: 20, y: 520, width: UIScreen.main.bounds.width - 20*2, height: 30))
-    var verificationcode: String = ""
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
@@ -25,18 +23,6 @@ class EConsentViewController: UIViewController {
         self.navigationItem.leftBarButtonItem?.tintColor = UIColor.black
         self.navigationItem.title = "eConsent"
 
-        code.callBacktext = { str in
-            //设置按钮的状态变化，但是次数问题很大，比如回退的时候会导致逻辑不通
-            self.code.textFiled.resignFirstResponder()
-            self.verificationcode  = str
-            self.nextbutton.backgroundColor = UIColor.init(displayP3Red: 236 / 255, green: 47 / 255, blue: 72 / 255, alpha: 1)
-            self.nextbutton.layer.borderWidth = 0
-            self.nextbutton.setTitleColor(UIColor.init(displayP3Red: 1, green: 1, blue: 1, alpha: 1), for: .normal)
-            self.nextbutton.isEnabled = true
-
-        }
-        //此处应该写一个删除验证码的时候 next的变化
-        view.addSubview(code)
     }
     @objc func onBackPressed() {
         self.navigationController?.popViewController(animated: true)
@@ -53,7 +39,7 @@ class EConsentViewController: UIViewController {
         guard let clinicalstudies = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "clinicalstudiesVC") as? ClinicalStudiesViewController else {
             return
         }
-//        self.navigationController?.pushViewController(clinicalstudies, animated: true)
+
         for vc in (self.navigationController?.viewControllers)! {
             if let clinicalVC  = vc as? ClinicalStudiesViewController {
                 self.navigationController?.popToViewController(clinicalVC, animated: true)
